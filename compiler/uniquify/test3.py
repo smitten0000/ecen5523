7dbb7305f756e21cf85dca6785b8a09e362dfecc
Brent Smith <brent.m.smith@colorado.edu>
2011-10-14 01:55:51 -0600

    - slight change to uniquify/test3.py

--- compiler/uniquify/test3.py
+++ compiler/uniquify/test3.py
@@ -1,9 +1,11 @@
 x = 1
 def a():
     return x
+print a()
 x = 2
 # SSA form
 # x_0 = 1
 # def a_0():
 #   return x   <-- do we refer to x_0 or x_1 here, or does it matter?  I think x_1, and it doesn't matter
+# print a_0()
 # x_1 = 2
